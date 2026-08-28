fecf786357178f6ee9a14d9fe27da6a6e36fdf4c
RathikrindiCharan648 <rathikrindicharan18@gmail.com>
2026-08-28 16:39:20 +0530

    changed to funtions

--- shop_billing_system.py
+++ shop_billing_system.py
@@ -16,32 +16,23 @@ products = {
 
 shopping_cart = {}
 
-print("""
-        =============== MENU ===============
-        1) view products in shop
-        2) add to cart
-        3) customer checkout
-        4) finish shopping
-    """)
-while True:
-    try:
-        user_selection = int(input('\nEnter your choice(from menu) : '))
-        if user_selection < 1 or user_selection > 4:
-            print('\nChoose in range (1 - 4) only!')
-            continue
-    except ValueError:
-        print('Invalid choose! Try again')
-        continue
+def menu():
+    return """
+    =============== MENU ===============
+            1) view products in shop
+            2) add to buying cart
+            3) customer checkout
+            4) finish shopping
+        """
 
-    if user_selection == 1:
-        print('\nThis is products list in shop')
-        print('  ')
-        for key, value in products.items():
-            print(f'  {key}  -   {value['price']}')
-        continue
-
-    elif user_selection == 2:
-        while True:
+def view_list():
+    output = ''
+    for item, item_details in products.items():
+        output += f'{item:<20} : ₹{item_details['price']:,.2f}\n'
+    return output
+    
+def add_cart():
+    while True:
             item_name = input("\nEnter product name : ").strip().lower()
             if not item_name.replace(' ','').isalpha():
                 print('Invalid product name! Try again')
@@ -50,50 +41,81 @@ while True:
                 print("This product is not there in this shop!")
                 continue
             else:
-                if item_name in products:
-                    while True:
-                        try:
-                            total_items = int(input("Enter how many products you want : "))
-                            if total_items < 1:
-                                print('Invalid number! Try again.')
-                                continue
-                            elif products[item_name]['stock'] == 0:
-                                print(f"\nInsufficient funds! No stock is available!")
-                                break
-                            elif total_items > products[item_name]['stock']:
-                                print(f"\nInsufficient funds! only {products[item_name]['stock']} is there!")
-                                continue
-                            else:
-                                products[item_name]['stock'] = products[item_name]['stock'] - total_items
-                                if item_name in shopping_cart:
-                                    shopping_cart[item_name] += total_items
-                                else:
-                                    shopping_cart[item_name] = total_items
-                                break
-                        except ValueError:
-                            print("Enter only numbers!")
-                            continue
-                    break
-    elif user_selection == 3:
-        if not shopping_cart:
-            print("Shopping cart is empty!")
+                break
+
+    if products[item_name]['stock'] == 0:
+        print(f"\nOut of stock! No stock is available for {item_name}!")
+        return
+
+    while True:
+        try:
+            total_items = int(input("Enter how many products you want : "))
+            if total_items < 1:
+                print('Invalid number! Try again.')
+                continue
+            elif total_items > products[item_name]['stock']:
+                print(f"\nInsufficient stock! only {products[item_name]['stock']} is there!")
+                continue
+            else:
+                products[item_name]['stock'] -= total_items
+                if item_name in shopping_cart:
+                    shopping_cart[item_name] += total_items
+                else:
+                    shopping_cart[item_name] = total_items
+                return f"\n{total_items} {item_name} added to cart successfully!"
+        except ValueError:
+            print("Enter only numbers!")
             continue
-        elif shopping_cart:
-            amount_list = []
-            for item, stocks in shopping_cart.items():
-                total_amount = products[item]['price'] * stocks
-                amount_list.append(total_amount)
-        if total_amount:
-            print(' ')
-            print(f'''
-------------------------------- Shopping Reciept ----------------------------------
-            ''')
-            for key, value in shopping_cart.items():
-                print(f'{key.capitalize()} * {value}  = {(products[key]['price']*value):,.2F}')
-            print(f'\n---------Grand Total --> {sum(amount_list):,.2F}---------')
-            print('\n =========================== THANKS FOR SHOPPING =============================')  
-            amount_list.clear()
-            shopping_cart.clear()
+    
+def billing():
+    if not shopping_cart:
+        print("Shopping cart is empty!")
+        return 
     else:
-        print("================= Finish shopping =================\n")
+        amount_list = []
+        for item, stocks in shopping_cart.items():
+            total_amount = products[item]['price'] * stocks
+            amount_list.append(total_amount)
+    if total_amount:
+        print(' ')
+        print(f'''
+------------------------------- Shopping Reciept ----------------------------------
+        ''')
+        for key, value in shopping_cart.items():
+            print(f'{key.capitalize()} * {value}  = {(products[key]['price']*value):,.2F}')
+        print(f'\n---------Grand Total --> {sum(amount_list):,.2F}---------')
+        print('\n =========================== THANKS FOR SHOPPING =============================')  
+        amount_list.clear()
+        shopping_cart.clear()
+
+def exit():
+    print("================= Finish shopping =================\n")
+    return
+
+def choose_menu():
+    while True:
+        try:
+            selection = int(input('\nEnter your choice(from menu) : '))
+            print(' ')
+            if selection < 1 or selection > 4:
+                print('\nChoose in range (1 - 4) only!')
+                continue
+        except ValueError:
+            print('Invalid choose! Try again')
+            continue
         break
+    return selection
+
+if __name__ == '__main__':
+    while True:
+        print(menu())
+        user_selection = choose_menu()
+        if user_selection == 1:
+            print(view_list())
+        elif user_selection == 2:
+            print(add_cart())
+        elif user_selection == 3:
+            billing()
+        else:
+            print(exit())
+            break
